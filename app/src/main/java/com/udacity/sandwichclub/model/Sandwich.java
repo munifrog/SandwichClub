e033667b54846106104a65f7cd32bdd45d89318e
Karl Arthur <karl.crispen.arthur@gmail.com>
2019-04-20 16:01:34 -0600

    feat: Parse sandwich JSON strings

--- app/src/main/java/com/udacity/sandwichclub/model/Sandwich.java
+++ app/src/main/java/com/udacity/sandwichclub/model/Sandwich.java
@@ -17,7 +17,14 @@ public class Sandwich {
     public Sandwich() {
     }
 
-    public Sandwich(String mainName, List<String> alsoKnownAs, String placeOfOrigin, String description, String image, List<String> ingredients) {
+    public Sandwich(
+            String mainName,
+            List<String> alsoKnownAs,
+            String placeOfOrigin,
+            String description,
+            String image,
+            List<String> ingredients
+    ) {
         this.mainName = mainName;
         this.alsoKnownAs = alsoKnownAs;
         this.placeOfOrigin = placeOfOrigin;
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,72 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
-public class JsonUtils {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+
+        try {
+            JSONObject object = new JSONObject(json);
+
+            JSONObject name = object.getJSONObject("name");
+
+            String sandwichName = (String) name.get("mainName");
+
+            List<String> sandwichAKAs = new ArrayList<>();
+            try {
+                JSONArray alsoKnowAsArray = name.getJSONArray("alsoKnownAs");
+                if (alsoKnowAsArray != null) {
+                    int length = alsoKnowAsArray.length();
+                    for(int i = 0; i < length; i++) {
+                        sandwichAKAs.add((String) alsoKnowAsArray.get(i));
+                    }
+                }
+            } catch (JSONException e) {
+                if (!e.getMessage().contains("No value for")) {
+                    e.printStackTrace();
+                }
+            }
+
+            String sandwichOrigin = (String) object.get("placeOfOrigin");
+
+            String sandwichDescription = (String) object.get("description");
+
+            String sandwichImage = (String) object.get("image");
+
+            List<String> sandwichIngredients = new ArrayList<>();
+            try {
+                JSONArray ingredientsArray = object.getJSONArray("ingredients");
+                if (ingredientsArray != null) {
+                    int length = ingredientsArray.length();
+                    for(int i = 0; i < length; i++) {
+                        sandwichIngredients.add((String) ingredientsArray.get(i));
+                    }
+                }
+            } catch (JSONException e) {
+                if (!e.getMessage().contains("No value for")) {
+                    e.printStackTrace();
+                }
+            }
+
+            sandwich = new Sandwich(
+                    sandwichName,
+                    sandwichAKAs,
+                    sandwichOrigin,
+                    sandwichDescription,
+                    sandwichImage,
+                    sandwichIngredients
+            );
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
     }
 }
