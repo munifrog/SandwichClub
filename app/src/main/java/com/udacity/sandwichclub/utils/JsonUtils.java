b31ceda58a5ac422a007661666f5b95c6595b95d
Karl Arthur <karl.crispen.arthur@gmail.com>
2019-04-24 22:29:27 -0600

    refactor: Use constants instead of hard-coded Strings

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -13,16 +13,25 @@ public class JsonUtils {
     public static Sandwich parseSandwichJson(String json) {
         Sandwich sandwich = null;
 
+        final String JSON_EMPTY_RETURN = "No value for";
+        final String SANDWICH_NAME_CONTAINER = "name";
+        final String SANDWICH_NAME_MAIN = "mainName";
+        final String SANDWICH_NAME_ALTERNATIVES = "alsoKnownAs";
+        final String SANDWICH_ORIGIN = "placeOfOrigin";
+        final String SANDWICH_DESCRIPTION = "description";
+        final String SANDWICH_IMAGE_URL = "image";
+        final String SANDWICH_INGREDIENTS = "ingredients";
+
         try {
             JSONObject object = new JSONObject(json);
 
-            JSONObject name = object.getJSONObject("name");
+            JSONObject name = object.getJSONObject(SANDWICH_NAME_CONTAINER);
 
-            String sandwichName = (String) name.get("mainName");
+            String sandwichName = (String) name.get(SANDWICH_NAME_MAIN);
 
             List<String> sandwichAKAs = new ArrayList<>();
             try {
-                JSONArray alsoKnowAsArray = name.getJSONArray("alsoKnownAs");
+                JSONArray alsoKnowAsArray = name.getJSONArray(SANDWICH_NAME_ALTERNATIVES);
                 if (alsoKnowAsArray != null) {
                     int length = alsoKnowAsArray.length();
                     for(int i = 0; i < length; i++) {
@@ -30,20 +39,20 @@ public class JsonUtils {
                     }
                 }
             } catch (JSONException e) {
-                if (!e.getMessage().contains("No value for")) {
+                if (!e.getMessage().contains(JSON_EMPTY_RETURN)) {
                     e.printStackTrace();
                 }
             }
 
-            String sandwichOrigin = (String) object.get("placeOfOrigin");
+            String sandwichOrigin = (String) object.get(SANDWICH_ORIGIN);
 
-            String sandwichDescription = (String) object.get("description");
+            String sandwichDescription = (String) object.get(SANDWICH_DESCRIPTION);
 
-            String sandwichImage = (String) object.get("image");
+            String sandwichImage = (String) object.get(SANDWICH_IMAGE_URL);
 
             List<String> sandwichIngredients = new ArrayList<>();
             try {
-                JSONArray ingredientsArray = object.getJSONArray("ingredients");
+                JSONArray ingredientsArray = object.getJSONArray(SANDWICH_INGREDIENTS);
                 if (ingredientsArray != null) {
                     int length = ingredientsArray.length();
                     for(int i = 0; i < length; i++) {
@@ -51,7 +60,7 @@ public class JsonUtils {
                     }
                 }
             } catch (JSONException e) {
-                if (!e.getMessage().contains("No value for")) {
+                if (!e.getMessage().contains(JSON_EMPTY_RETURN)) {
                     e.printStackTrace();
                 }
             }
